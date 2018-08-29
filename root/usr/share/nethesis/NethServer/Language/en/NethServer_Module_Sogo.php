31af9cbd826e17204789c0eecfa68c998665cae3
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-08-30 00:00:18 +0200

    Language configuration

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Sogo.php
@@ -0,0 +1,18 @@
+<?php
+$L['Sogo_header'] = 'Configuration';
+$L['Sogo_Title'] = 'SOGo';
+$L['Sogo_Description'] = 'SOGo is the groupware solution';
+$L['status_label'] = 'Enable SOGo';
+$L['Dav_label'] = 'Enable CalDAV and CardDAV';
+$L['MailAuxiliaryUserAccountsEnabled_label'] = 'Allow Users to add other IMAP accounts';
+$L['ActiveSync_label'] = 'Enable Microsoft ActiveSync';
+$L['AdminUsers_label'] = 'Administrators';
+$L['Advanced_label'] = 'Advanced settings';
+$L['VirtualHost_label'] = 'Make SOGO accessible only from this domain name';
+$L['Notifications_label'] = 'Notifications';
+$L['ACLs_label'] = 'Sent by emails to involved users of a calendar/address book';
+$L['Folders_label'] = 'Sent by emails on the creation of a calendar/address book.';
+$L['Appointment_label'] = 'Sent by emails to meeting participants';
+$L['EMail_label'] = "Sent by emails to user's events and tasks";
+$L['WOWorkersCount_label'] = 'Number of workers (one per activesync client connected)';
+$L['Must be a user name'] = "It must be a valid user name";
